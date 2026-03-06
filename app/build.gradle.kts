4ad87efee8022148845258a5ff1e3b591570f939
Nicos Nicolaou <nicosnicos16@gmail.com>
2026-03-06 23:51:09 +0200

    minor fix

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -35,13 +35,15 @@ android {
         sourceCompatibility = JavaVersion.VERSION_21
         targetCompatibility = JavaVersion.VERSION_21
     }
-    kotlin {
-        compilerOptions {
-            jvmTarget = JvmTarget.fromTarget("21")
-        }
-    }
     buildFeatures {
         compose = true
+    }
+}
+
+kotlin {
+    compilerOptions {
+        languageVersion = org.jetbrains.kotlin.gradle.dsl.KotlinVersion.KOTLIN_2_3
+        jvmTarget = JvmTarget.fromTarget("21")
     }
 }
 
