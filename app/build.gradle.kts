d16d1bc9e2fda59191aa7c3cbdbd6add2b8080f0
Nicos Nicolaou <nicosnicos16@gmail.com>
2026-08-16 01:13:25 +0300

    update Target SDK to 37

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,7 +15,7 @@ android {
     defaultConfig {
         applicationId = "com.nicos.trusttimeapiimplementation"
         minSdk = 29
-        targetSdk = 36
+        targetSdk = 37
         versionCode = 1
         versionName = "1.0"
 
