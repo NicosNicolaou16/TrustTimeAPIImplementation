e647c7437508aa9194f5a580b2f1850f842ef312
Nicos Nicolaou <nicosnicos16@gmail.com>
2026-08-16 01:13:03 +0300

    update Target SDK to 37

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -9,7 +9,7 @@ plugins {
 
 android {
     namespace = "com.nicos.trusttimeapiimplementation"
-    buildToolsVersion = "36.0.0"
+    buildToolsVersion = "37.0.0"
     compileSdk = 36
 
     defaultConfig {
